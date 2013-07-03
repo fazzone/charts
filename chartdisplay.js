a216e4812aa27af413c03b1de041ced107a2997c
Russell McQueeney <rmcq@utexas.edu>
2013-07-03 02:09:08 -0400

    reworked to lift the rendering logic out into an object, improvement

--- chartdisplay.js
+++ chartdisplay.js
@@ -1,13 +1,14 @@
 /*
 The chart schema is a JSON object that will look (for example) sort of like this
+Note that we sort of have the CSS analogue of Greenspun's Tenth Rule going on here
 */
 sch_top50 = cascade({
 	name: "Top 50",
-	header_font: "26px Sans",
-	font_height: 20, //estimate
+	font: "26px Sans",
+	font_height: 26, //estimate
 	text_padding_bottom: 10, //how much vertical space to put between the label text and first row
 	text_padding_left: 40, //how much horiz. space between the label and x=0
-	text_padding_top: 10,
+	text_padding_top: 24,
 	text_fill: "white",
 	bg_fill: "rgb(36, 36, 36)",
 	margin_bottom: 40, //how much vertical space to add after each row
@@ -34,6 +35,14 @@ sch_top50 = cascade({
 		placeholder_fill: "#02a1e5"
 	}]});
 
+//a Renderer is an object with a renderCell function as such
+r_placeholder = {
+	renderCell: function(ctx, sch, x, y) {
+		ctx.fillStyle = sch.placeholder_fill;
+		ctx.fillRect(x, y, sch.picsize, sch.picsize);
+	}
+}
+
 function cascade(obj, parent) {
 	for (var v in obj) {
 		if (obj.hasOwnProperty(v) && obj[v] !== obj.rows)
@@ -52,18 +61,20 @@ function calcAlbumsPerRow(canvas, r) {
 }
 
 function calcRowHeight(canvas, r) {
-	var est_fontHeight = r.font_height;
-	return est_fontHeight + r.margin_bottom + (r.picsize + r.picpadding) * Math.ceil(r.n_albums / calcAlbumsPerRow(canvas, r))
+	if (r.n_albums == 0)
+		return r.font_height + r.text_padding_top;
+	return r.text_padding_top + r.margin_bottom + (r.picsize + r.picpadding) * Math.ceil(r.n_albums / calcAlbumsPerRow(canvas, r))
 }
 
 function calcCanvasSize(sch) {
-	var maxW = 0, extentY = 20;
+	var maxW = 0, extentY = sch.text_padding_top;
 	for (var i=0; i<sch.rows.length; i++) {
 		var r = sch.rows[i];
 		//got to subtract picpadding because we count it twice
 		maxW = Math.max(maxW, 2*r.margin_side + (r.picsize + r.picpadding) * calcAlbumsPerRow(null, r) - r.picpadding);
 		extentY += calcRowHeight(null, r);
 	}
+	console.log(maxW, extentY);
 	return {
 		width: maxW,
 		height: extentY
@@ -71,39 +82,45 @@ function calcCanvasSize(sch) {
 }
 
 function renderChartPlaceholder(canvas, sch) {
+	g_renderChart(canvas, sch, renderRowPlaceholder);
+}
+
+function g_renderChart(canvas, sch, rowfunc) {
 	var ctx = canvas.getContext("2d");
-	ctx.font = sch.header_font;
+
 	ctx.fillStyle = sch.bg_fill;
 	ctx.fillRect(0, 0, canvas.width, canvas.height);
-	var cRowY = 20;
+	var cRowY = sch.text_padding_top;
 	for (var i=0; i<sch.rows.length; i++) {
-		renderRowPlaceholder(canvas, ctx, sch.rows[i], cRowY);
+		rowfunc(canvas, ctx, sch.rows[i], cRowY);
 		cRowY += calcRowHeight(canvas, sch.rows[i]);
-		console.log(cRowY);
 	}		
 }
 
-function renderRowPlaceholder(canvas, ctx, r, base_y) {
+function g_renderRow(canvas, ctx, renderer, r, base_y) {
 	base_y += r.text_padding_top;
 
 	ctx.fillStyle = r.text_fill;
+	ctx.font = r.font;
 	ctx.fillText(r.name, r.text_padding_left, base_y);
-
+	
 	base_y += r.text_padding_bottom;
 	ctx.fillStyle = r.placeholder_fill;
-	
 	
 	var apr = calcAlbumsPerRow(canvas, r);
 	var tsize = r.picsize + r.picpadding; //tile size
 	var x = 0, y = 0;
 	for (var i=0; i<r.n_albums; i++) {
-		ctx.fillRect(r.margin_side + x * tsize, base_y + y * tsize, r.picsize, r.picsize);
+ 		renderer.renderCell(ctx, r, r.margin_side + x * tsize, base_y + y * tsize);
+
 		x++;
 		if (x >= apr) {
 			y++;
 			x = 0;
-		}
-			
+		}		
 	}
 }
 
+function renderRowPlaceholder(canvas, ctx, r, base_y) {
+	g_renderRow(canvas, ctx, r_placeholder, r, base_y);
+}
